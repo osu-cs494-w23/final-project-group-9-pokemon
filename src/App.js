f66052c49882ee3f66be60efd3a6e2f05a66755f
spcurtis13 <>
2023-03-21 10:34:36 -0700

    fixed issue with loading api now skeleton is done for pokemon and moves need to make sure its what team expects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Home from './pages/Home'
 import {Pokemon, SinglePokemon} from './pages/Pokemon'
+import Moves from './pages/Moves'
+import Move from './pages/Move'
 
 function App() {
     return (
@@ -17,6 +19,14 @@ function App() {
             >
                 <Route path=":pokemonName" element={<SinglePokemon />} />
             </Route>
+            <Route
+                path="/moves"
+                element={<Moves />}
+            />
+            <Route 
+                path="/moves/:moveName" 
+                element={<Move />} 
+            />
             <Route path="/" element={<Navigate to="/home" />} />
         </Routes>
     )
--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -13,28 +13,50 @@ function SinglePokemon(){
     // console.log(usePokemonSearch(pokemonName.species));
     const [pokemon, loading, error] = usePokemonSearch(pokemonName);
     console.log(pokemon.battle);
-    const length = pokemon.species.flavor_text_entries.length
-    const typeLength = pokemon.battle.types.length
-    let types = []
-    types.push(pokemon.battle.types[0].type.name)
-    if (typeLength === 2){
-        types.push(pokemon.battle.types[1].type.name)
-    }
-    return (
+    if (pokemon.battle.types){
+        const length = pokemon.species.flavor_text_entries.length
+        const typeLength = pokemon.battle.types.length
+        const types = []
+        types.push(pokemon.battle.types[0].type.name)
+        if (typeLength === 2){
+            types.push(pokemon.battle.types[1].type.name)
+        }
+        return (
         <>
             <div>
                 <h1>#{pokemon.battle.id}</h1>
                 <h1>{pokemon.battle.name}</h1>
-                <h2>{types}</h2>
+                <h2></h2>
                 <h3>Most Recent Dex Entry:</h3>
-                <h4>{pokemon.species.flavor_text_entries[length-1].flavor_text}</h4>
+                <h4>{pokemon.species.flavor_text_entries[length -1].flavor_text}</h4>
             </div>
             <div>
                 <h2>Battle Data:</h2>
-                {/* /* <h3>HP: {pokemon.battle.</h3> */}
+                <h3>HP: {pokemon.battle.stats[0].base_stat}</h3>
+                <h3>Attack: {pokemon.battle.stats[1].base_stat}</h3>
+                <h3>Defence: {pokemon.battle.stats[2].base_stat}</h3>
+                <h3>Special Attack: {pokemon.battle.stats[3].base_stat}</h3>
+                <h3>Special Defence: {pokemon.battle.stats[4].base_stat}</h3>
+                <h3>Speed: {pokemon.battle.stats[5].base_stat}</h3>
+            </div>
+            <div>
+                <h2>Ability Data:</h2>
+                {pokemon.battle.abilities.map((ability, index) => (
+                    <h3 key={index}>{ability.ability.name}</h3>
+                ))}
+            </div>
+            <div>
+                <h2>Move Set: </h2>
+                {pokemon.battle.moves.map((moves, index) => (
+                    <h3 key={index}>{moves.move.name}</h3>
+                ))}
             </div>
         </>
-    )
+        )
+    } else {
+        return <h1>Loading...</h1>
+    }
+    
 
 }
 function Pokemon() {
