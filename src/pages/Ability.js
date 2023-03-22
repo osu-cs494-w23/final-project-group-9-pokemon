0c603eaa8cbfc32cfc650f1c93d59efde3795849
Min Khant Aung <minkhant@Mins-MacBook-Pro.local>
2023-03-22 15:36:02 -0700

    Abilities and Ability endpoints are added

---
+++ src/pages/Ability.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { Outlet, useParams, useSearchParams } from "react-router-dom";
+
+import useSpecificAbilitySearch from "../hooks/useSpecificAbilitySearch";
+
+function Ability() {
+    const { abilityName } = useParams()
+
+    const [ abilitydata, loading, error ] = useSpecificAbilitySearch(abilityName)
+    console.log(abilitydata)
+
+
+    if (abilitydata.effect_entries) {
+        return (
+            <>
+                <div>
+                    <h1>{abilityName}</h1>
+                    <h2>{abilitydata.effect_entries[1].effect}</h2>
+                </div>
+            </>
+        )
+    } else {
+        return <h1>Loading...</h1>
+    }
+}
+
+export default Ability
