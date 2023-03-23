660be24f7df125e22bb55ad8ba46fe5ca0a315be
busteeda <busteeda@oregonstate.edu>
2023-03-23 15:50:04 -0700

    Updated pokedex page

--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -10,14 +10,27 @@ import pokemonJson from '../data/pokemon.json'
 import { Pokemon } from './Pokemon'
 
 const pokemonData = pokemonJson
+var searchQuery
+
+function SearchBar() {
+    return (
+        <form  
+            onSubmit={e => {
+            e.preventDefault()
+            }}>
+                <input placeholder="Search for a Pokemon..." />
+                <button type="submit">Search</button>
+        </form>
+    )
+}
 
 function PokemonCard(props) {
     return (
             <div className='pokemonCard'>
+                <p className='dexNumber'>#{props.dexNumber}</p>
                 <div className='imgcontainer'>
                     <img className='pokemonImg' src={props.image}/>
                 </div>
-                <p className='dexNumber'>#{props.dexNumber}</p>
                 <p className='pokemonName'>{props.pokemonName}</p>
                 <div className='typeContainer'>
                     <div className='type'>
@@ -37,9 +50,11 @@ function Pokedex() {
     return (
         <>
             <Topnav/>
+            <SearchBar/>
             <div className='pokemonList'>
-            {Object.keys(pokemonData).map(key => (
-                <NavLink key={key} to={`/pokemon${key}`}>
+            {Object.keys(pokemonData.filter(PokemonCard => PokemonCard.pokemonName.includes('C'))).map(key => (
+            /* {Object.keys(pokemonData).map(key => ( */
+                <NavLink className='pokemonLink' key={key} to={`/pokemon/${key}`}>
                     <PokemonCard
                     pokemonName={pokemonData[key].name}
                     dexNumber={pokemonData[key].dexNum}
