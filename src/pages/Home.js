77a0b2f7d8244cc780f8d9eb9aab70dce61406e7
Conner <rheac@oregonstate.edu>
2023-03-23 16:59:10 -0700

    fixed minor issue with Database + removed comments in Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,19 +14,6 @@ import { NavLink } from 'react-router-dom'
 // {"dexNum": , "name": "", "typeOne": "", "typeTwo": "", "photoUrl": "https://img.pokemondb.net/sprites/black-white/normal/.png"}
 
 function Home() {
-    // I'm just testing things here, feel free to overwrite this page and its elements - Conner
-
-    // const [ pokemon, loading, error ] = usePokemonSearch("1")
-    // const [ movelist, mLoading, mError ] = useMoveSearch()
-    // const [ movedata, smLoading, smError ] = useSpecificMoveSearch("1")
-    //const [ abilitylist, aLoading, aError ] = useAbilitySearch()
-    //const [ abilityData, saLoading, saError] = useSpecificAbilitySearch("2")
-
-    // console.log("Pokemon Data: ", pokemon)
-    // console.log("Movelist Data: ", movelist)
-    // console.log("Specific Move Data: ", movedata)
-    //console.log("Abilitylist Data: ", abilitylist)
-    //console.log("Specific Ability Data: ", abilityData)
 
     let audio = new Audio("/eevee.mp3")
 
@@ -43,7 +30,7 @@ function Home() {
                 <ul className="home_style">
                     <li className="home_style"><NavLink to="/pokedex" className="home_style">PokéDex</NavLink></li>
                     <li className="home_style"><NavLink to="/abilities" className="home_style">AbilityDex</NavLink></li>
-                    <li className="home_style"><NavLink to="/movedex" className="home_style">MoveDex</NavLink></li>
+                    <li className="home_style"><NavLink to="/moves" className="home_style">MoveDex</NavLink></li>
                 </ul>
             </div>
         </>
